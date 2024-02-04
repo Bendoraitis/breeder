26595f506f60588f33ddd8769caf35fbc1552e6f
rimas <rimas@rimas.lt>
2024-02-04 21:09:48 +0200

    Save Project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # dog breeder by picture shredding
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
